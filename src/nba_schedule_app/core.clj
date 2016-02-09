7d4817d1a371369deae6a618ca93a52930c39f5e
dave <dkraytsberg@gmail.com>
2016-02-09 07:05:32 -0500

    added score querying for games from json

--- src/nba_schedule_app/core.clj
+++ src/nba_schedule_app/core.clj
@@ -20,10 +20,13 @@
         (catch 
           Exception e (println "server request error -- check your parameters, such as date")))))
 
+(defn get-schedule-results [date]
+  (get (get-schedule date) "resultSets"))
 
-(defn map-names [schedule]
-  (let [names (map #(get %1 "name") schedule)]
-    (zipmap names schedule)))
+(defn map-names-over-tables [tables]
+  "return a map with each table's name as the key and the table as value"
+  (let [names (map #(get %1 "name") tables)]
+    (zipmap names tables)))
 
 (defn team-id-map [schedule]
   "return of a mapping from nba team ids to their cities"
@@ -32,7 +35,7 @@
       (let [team-data (zipmap headers team)]
         (swap! teams assoc (get team-data "TEAM_ID") (get team-data "TEAM")))))
   (let [teams (atom {})
-        tables (map-names schedule)
+        tables (map-names-over-tables schedule)
         east (get tables "EastConfStandingsByDay")
         west (get tables "WestConfStandingsByDay")
         headers (get east "headers")]
@@ -44,7 +47,7 @@
   (let [matchups (atom [])
         schedule (get (get-schedule date) "resultSets")
         team-ids (team-id-map schedule)
-        games (get (map-names schedule) "GameHeader")
+        games (get (map-names-over-tables schedule) "GameHeader")
         headers (get games "headers")]
     (doseq [matchup (get games "rowSet")]
       (let [matchup-data (zipmap headers matchup)
@@ -56,23 +59,81 @@
         (swap! matchups conj [visit "@" home status (or tv "Local TV")])))
     @matchups))
 
+(defn get-games-2 [date]
+  (let [results (atom [])
+        schedule (get-schedule-results date)
+        team-ids (team-id-map schedule)
+        games (get (map-names-over-tables schedule) "GameHeader")
+        headers (get games "headers")]
+    (doseq [matchup (get games "rowSet")]
+      (let [matchup-data (zipmap headers matchup)
+            {game-id "GAME_ID"
+             home-id "HOME_TEAM_ID" 
+             visit-id "VISITOR_TEAM_ID" 
+             status "GAME_STATUS_TEXT"
+             tv "NATL_TV_BROADCASTER_ABBREVIATION"} matchup-data
+            {home home-id 
+             visit visit-id} team-ids
+             game-map {:game-id game-id
+                       :visit-id visit-id
+                       :visit visit
+                       :home-id home-id
+                       :home home
+                       :status status
+                       :tv (or tv "Local Brodcast")}]
+        (swap! results conj game-map)))
+    @results))
+
+
+(defn get-scores [date]
+  "return a mapping from nba game ids to their score data"
+  (let [results (atom [])
+        schedule (get-schedule-results date)
+        team-ids (team-id-map schedule)
+        scores (get (map-names-over-tables schedule) "LineScore")
+        headers (get scores "headers")]
+    (doseq [game-scores (get scores "rowSet")]
+      (let [score-data (zipmap headers game-scores)
+            {game-id "GAME_ID"
+             team-id "TEAM_ID"
+             pts "PTS"
+             fg% "FG_PCT"
+             ft% "FT_PCT"
+             fg3% "FG3_PCT"
+             ast "AST"
+             reb "REB"} score-data
+            {team team-id} team-ids
+            score-map {:game-id game-id
+                       :team-id team-id
+                       :team team
+                       :pts pts
+                       :fg% fg%
+                       :fg3% fg3%
+                       :ast ast
+                       :reb reb}]
+        (swap! results conj score-map)))
+    @results))
+
+(defn merge-games-with-scores [games scores]
+  (let [results (atom [])]
+    (doseq [game games]
+      (assoc games (filter #(
+
+
 (defn game-contains-team? [team]
   (fn [[away at home]] (or (= team away) (= team home))))
 
 
 
 (defn -main
-
   ([]
     (-main (today)))
-
   ([date]
    (let [games (get-games date)]
      (if (empty? games) 
        (println "no games scheduled")
        (doseq [g games]
          (println g)))))
-
   ([date team]
    (let [games (get-games date)
          team-games (filter (game-contains-team? team) games)]
